58bcecb1791b13e2f58286f8f5b4ed53f4e73e93
11245 <11245>
2020-07-13 23:06:55 +0500

    update lowpass converter

--- scripts/convert_hipass.sh
+++ scripts/convert_hipass.sh
@@ -11,7 +11,7 @@ for f in "$DIR"/*/*/*; do
       destfile="$filename.dest.wav"
 
       echo $sourcefile;
-      ffmpeg -loglevel error -y -i $sourcefile -vn -ar 8000 -ac 1 -filter:a "highpass=f=100, lowpass=f=2700" $destfile
+      ffmpeg -loglevel error -y -i $sourcefile -vn -ar 8000 -ac 1 -filter:a "highpass=f=100, lowpass=f=2000" $destfile
       ffmpeg -loglevel error -y -i $destfile -v quiet -acodec pcm_s16le -ar 8000 -ac 1 $sourcefile
       rm $destfile
       ) &
